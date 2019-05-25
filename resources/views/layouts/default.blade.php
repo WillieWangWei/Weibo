0f4842873c8a82868d0a67cbf3e1003e9ed56e5b
Willie <willie.wangwei@gmail.com>
2019-05-25 18:35:12 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
-<html lang="">
-<head>
-  <title>@yield('title', 'Weibo App') - Laravel</title>
-</head>
-<body>
-	@yield('content')
-</body>
+<html>
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Weibo App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item"><a class="nav-link" href="/#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
 </html>
