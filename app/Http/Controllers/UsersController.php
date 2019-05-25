e95625e05a2d982e9b4291025fef99c15e938a37
Willie <willie.wangwei@gmail.com>
2019-05-25 20:25:07 +0800

    用户注册页面

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function create() {
+        return view('users.create');
+    }
+}
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+
+@section('title', '注册');
+
+@section('content')
+  <h1>注册</h1>
+@stop
+
