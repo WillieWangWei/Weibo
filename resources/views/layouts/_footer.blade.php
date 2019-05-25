25b3e15dd10152b505b8b7d870837e722b06dce1
Willie <willie.wangwei@gmail.com>
2019-05-25 20:08:02 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
     刻意练习，每日精进
   </a>
   <div class="float-right">
-    <a href="/about">关于</a>
+    <a href="{{ route('about') }}">关于</a>
   </div>
 </footer>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');;
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');;
