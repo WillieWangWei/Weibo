4a4793c272fe6f74439c692e5c1631ae017645fe
Willie <willie.wangwei@gmail.com>
2019-05-25 16:29:14 +0800

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+  <title>@yield('title', 'Weibo App') - Laravel</title>
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '主页')
+@section('content')
+  <h1>主页</h1>
+@stop
