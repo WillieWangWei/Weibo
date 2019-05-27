4626d2c032d339c8453c08da7c456e8c0d414b0e
Willie <willie.wangwei@gmail.com>
2019-05-27 19:29:49 +0800

    Bug fix.

--- public/js/app.js
+++ public/js/app.js
@@ -49437,8 +49437,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/willie/Code/weibo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/willie/Code/weibo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/Code/weibo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/Code/weibo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
