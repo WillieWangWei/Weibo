4a61d46748839ba9fc66d2ff3be18f406ffbebb0
Willie <willie.wangwei@gmail.com>
2019-06-01 18:58:52 +0800

    Social statistics.

---
+++ database/seeds/FollowersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class FollowersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::all();
+        $user = $users->first();
+        $user_id = $user->id;
+
+        // 获取去除掉 ID 为 1 的所有用户 ID 数组
+        $followers = $users->slice(1);
+        $follower_ids = $followers->pluck('id')->toArray();
+
+        // 关注除了 1 号用户以外的所有用户
+        $user->follow($follower_ids);
+
+        // 除了 1 号用户以外的所有用户都来关注 1 号用户
+        foreach ($followers as $follower) {
+            $follower->follow($user_id);
+        }
+    }
+}
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,6 +8,9 @@
       <section class="user_info">
         @include('shared._user_info', $user)
       </section>
+      <section class="stats mt-2">
+        @include('shared._stats', $user)
+      </section>
 
       <section class="status">
         @if($statuses->count() > 0)
