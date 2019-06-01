bd120376c0c89cb4b684a96396ab5c9f7cc91f89
Willie <willie.wangwei@gmail.com>
2019-06-02 00:11:29 +0800

    Add feed from followings.

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,8 +63,10 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this
-            ->statuses()
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
             ->orderBy('created_at', 'desc');
     }
 
